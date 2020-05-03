18a31138eaa4da416ad3d7c96942bed188fad2d5
John FitzGerald <jontsnz@gmail.com>
2020-05-03 23:25:33 +1000

    Use settings from config file

--- influx-ingester.py
+++ influx-ingester.py
@@ -13,8 +13,8 @@ import json
 import paho.mqtt.client as mqtt
 import requests
 
-def setup_client(host, port, topic):
-    client = mqtt.Client()
+def setup_client(host, port, topic, userdata):
+    client = mqtt.Client(userdata=userdata)
     client.on_message = on_message
     keepalive = 60
     client.connect(host, port, keepalive)
@@ -22,37 +22,39 @@ def setup_client(host, port, topic):
     return client
 
 def on_message(client, userdata, message):
-    msg = str(message.payload.decode("utf-8"))
-    print("message received " , msg)
+    msg = str(message.payload.decode('utf-8'))
+    silent_mode = userdata['silent']
+    if not silent_mode:
+        print('message received ' , msg)
     json_data = json.loads(msg)
-    send_data_to_influx(json_data)
+    send_data_to_influx(json_data, silent_mode, userdata['config'])
 
-def send_data_to_influx(json_data):
-    line_data = convert_json_to_linedata(json_data)
-    api_url = 'http://localhost:9999/api/v2/write'
-    org = 'CCP'
-    bucket = 'water-quality'
-    precision = 'us'
-    token = 'e8LDuebi8FoDpeFsNxkyTwxAcGogXns6RUKrIGPHZ7Pr4jD1rq7vm54sS1cUD9NQBhBuGRGrPrEwBIJ7ok2xfA=='
+def send_data_to_influx(json_data, silent_mode, config):
+    line_data = convert_json_to_linedata(json_data, config)
+    api_url = config['destination']['influxdb']['api_endpoint']
+    org = config['destination']['influxdb']['org']
+    bucket = config['destination']['influxdb']['bucket']
+    precision = config['destination']['influxdb']['precision']
+    token = config['destination']['influxdb']['token']
     url = '%s?org=%s&bucket=%s&precision=%s' % (api_url, org, bucket, precision)
     header = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": "Token %s" % (token) }
-    print('POSTing to %s linedata %s' % (url,line_data))
+    if not silent_mode:
+        print('POSTing to %s linedata %s' % (url,line_data))
     try:
         response = requests.post(url,data=line_data, headers=header, verify=False)
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
-    print('API response: %d %s' % (response.status_code, response.reason))
+    if not silent_mode:
+        print('API response: %d %s' % (response.status_code, response.reason))
 
-def convert_json_to_linedata(json_data):
-    # TODO write this code
-    # {"TIMESTAMP": "2020-04-27 11:46:24.922982", "RECORD": 44, "Station": "DummyRiverWQ", "LoggerBattV": 12.7, "EXO_TempC": 24.64, "EXO_pH": 6.73, "EXO_DOPerSat": 22.05, "EXO_TurbNTU": 28.45, "EXO_Depthm": 1.558}
-    # example format: mem,host=host1 used_percent=23.43234543 1556896326
-    # example format: sensors,station=DummyRiverWQ RECORD=44,LoggerBattV=12.7,EXO_TempC=24.64,EXO_pH=6.73,EXO_TurbNTU=28.45,EXO_Depthm=1.558 1556896326
+def convert_json_to_linedata(json_data, config):
+    # example json:     {"TIMESTAMP": "2020-04-27 11:46:24.922982", "RECORD": 44, "Station": "DummyRiverWQ", "LoggerBattV": 12.7, "EXO_TempC": 24.64, "EXO_pH": 6.73, "EXO_DOPerSat": 22.05, "EXO_TurbNTU": 28.45, "EXO_Depthm": 1.558}
+    # example limedata: sensors,station=DummyRiverWQ RECORD=44,LoggerBattV=12.7,EXO_TempC=24.64,EXO_pH=6.73,EXO_TurbNTU=28.45,EXO_Depthm=1.558 1556896326
     station_name = json_data['Station']
     timestamp = json_data['TIMESTAMP']
-    from_zone = tz.gettz('UTC')
-    to_zone = tz.gettz('AEST')
-    from_dt = datetime.strptime(timestamp,'%Y-%m-%d %H:%M:%S.%f')
+    from_zone = tz.gettz(config['source']['timezone'])
+    to_zone = tz.gettz(config['destination']['timezone'])
+    from_dt = datetime.strptime(timestamp,config['source']['timestamp_format'])
     from_dt = from_dt.replace(tzinfo=from_zone)
     to_dt = from_dt.astimezone(to_zone)
     timestamp_nano = '{0:.0f}'.format(to_dt.timestamp() * 1000000)
@@ -66,6 +68,7 @@ def main(arguments):
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-c', '--configfile', help="Config file")
+    parser.add_argument('--silent', action='store_true', help="Silent mode")
     args = parser.parse_args(arguments)
 
     if args.configfile:
@@ -77,12 +80,19 @@ def main(arguments):
     else:
         print('Config file must be provided')
 
+    if args.silent:
+        print('SILENT mode')
+        silent_mode = True
+    else:
+        silent_mode = False
+
     if config['source']['mqtt']:
         host = config['source']['mqtt']['hostname']
         port = config['source']['mqtt']['port']
         topic = config['source']['mqtt']['topic']
         print('Waiting for data from MQTT %s:%s on %s' % (host, port, topic))
-        mqtt_client = setup_client(host, port, topic)
+        userdata = {'config': config, 'silent': silent_mode}
+        mqtt_client = setup_client(host, port, topic, userdata)
     else:
         print('No MQTT source configured. Exiting.')
 
